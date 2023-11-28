0316d5b0fdf12e0aa5a57125184c0a0800e93965
aangniu <aangniu96@gmail.com>
2023-11-27 16:38:30 -0800

    add generating mesh for stepover

---
+++ stepover/generating_the_mesh.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+prefix=stepover_Nc
+# Generate the mesh using gmsh
+#for gmsh, see http://gmsh.info/#Download
+gmsh -3 -algo hxt $prefix.geo
+
+# Convert the mesh from neu to hdf5 using pumgen
+# for pumgen, see https://github.com/SeisSol/PUMGen/wiki/How-to-compile-PUMGen
+pumgen $prefix.msh -s msh2
