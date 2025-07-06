640c3676ca9941d70d9b4be3d615661e4266a09e
aangniu <aangniu96@gmail.com>
2025-07-06 22:58:17 +0200

    add inputs for cdb benchmark

---
+++ tpv5_benchmark/test_benchmark.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+# Job Name and Files (also --job-name)
+#SBATCH -J LD_CDB
+#Output and error (also --output, --error):
+#SBATCH -o ./%j.%x.out
+#SBATCH -e ./%j.%x.err
+
+#Initial working directory:
+#SiBATCH --chdir=./
+
+#Notification and type
+#SBATCH --mail-type=END
+#SBATCH --mail-user=zihua.niu@lmu.de
+
+# Wall clock limit:
+#SBATCH --no-requeue
+
+#Setup of execution environment
+#SBATCH --export=ALL
+#SBATCH --account=pn49ha
+#constraints are optional
+#--constraint="scratch&work"
+#SBATCH --partition=general
+
+#SBATCH --ntasks-per-node=1
+#SBATCH --nodes=64
+#SBATCH --time=01:30:00
+#SBATCH --ear=off
+
+# module load slurm_setup
+
+#Run the program: 
+export MP_SINGLE_THREAD=no
+unset KMP_AFFINITY
+
+export OMP_NUM_THREADS=92
+export OMP_PLACES="cores(46)"
+#export I_MPI_SHM_HEAP_VSIZE=24576
+
+export XDMFWRITER_ALIGNMENT=8388608
+export XDMFWRITER_BLOCK_SIZE=8388608
+export SC_CHECKPOINT_ALIGNMENT=8388608
+
+export SEISSOL_CHECKPOINT_ALIGNMENT=8388608
+export SEISSOL_CHECKPOINT_DIRECT=0
+export ASYNC_MODE=THREAD
+export ASYNC_BUFFER_ALIGNMENT=8388608
+export SEISSOL_ASAGI_MPI_MODE=OFF
+source /etc/profile.d/modules.sh
+
+echo $SLURM_NTASKS
+ulimit -Ss 2097152
+#mpiexec -n $SLURM_NTASKS /hppfs/work/pr63qo/di73yeq4/SeisSol/build-release/SeisSol_Release_dskx_4_viscoelastic2 parameters_M6.6_m1.0HzTopo_o4visc.par
+#mpiexec -n $SLURM_NTASKS /hppfs/work/pr63qo/di73yeq4/SeisSol/build-release/SeisSol_Release_dskx_4_viscoelastic2 parameters_M7.3_m1.0HzTopo_o4visc.par
+# mpiexec -n $SLURM_NTASKS /dss/dsshome1/0F/ra75qeh2/seisNonl/seisDrIS/SeisSol/build_asagi/SeisSol_Release_dskx_4_damaged-elastic parameters_o4CoarseDam34.par
+mpiexec -n $SLURM_NTASKS /dss/dsshome1/0F/ra75qeh2/seisNonl/seisBench/SeisSol/build_test/SeisSol_Release_dskx_2_damaged-elastic parametersBenchmark.par
+#mpiexec -n $SLURM_NTASKS /hppfs/work/pr63qo/di73yeq4/SeisSol/build-release/SeisSol_Release_dskx_4_viscoelastic2 parameters_M7.3_m1.0Hz5kmTopo_o4visc.par
+#mpiexec -n $SLURM_NTASKS /hppfs/work/pr63qo/di73yeq4/SeisSol/build-gcc/SeisSol_Debug_dskx_4_viscoelastic2 parameters_M7.3_m1.0HzTopo_o4visc.par
+#mpiexec -n $SLURM_NTASKS /hppfs/work/pr63qo/di73yeq4/SeisSol/build-gcc/SeisSol_Release_dskx_4_viscoelastic2 parameters_M7.3_m1.0HzTopo_o4visc.par
